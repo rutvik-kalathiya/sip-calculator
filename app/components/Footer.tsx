5673ff0a0226631f5c2fad9113a631e13956e5d4
rutvikkalathiya0 <rutvikkalathiya0@gmail.com>
2025-03-16 10:28:46 +0000

    Footer added

---
+++ app/components/Footer.tsx
@@ -0,0 +1,20 @@
+"use client";
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <footer className="w-full py-4 bg-gray-100 dark:bg-gray-900 h-[10vh]">
+      <p className="text-gray-700 dark:text-gray-300 h-full flex items-center justify-center">
+        View the source code on 
+        <Link
+          href="https://github.com/rutvik-kalathiya/sip-calculator" 
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-600 dark:text-blue-400 hover:underline ml-1"
+        >
+          GitHub
+        </Link>
+      </p>
+    </footer>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
+import Link from "next/link";
 import InvestmentCalculator from "./components/InvestmentCalculator";
 import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
     <div>
       <NavBar />
       <InvestmentCalculator />
+      <Footer />
     </div>
   );
 }
